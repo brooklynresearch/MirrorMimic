2d8202e70bf79488ad104517e2bfd1e6d6967f13
EzerLonginus <ezer@brooklynresearch.org>
2016-06-21 17:29:23 -0400

    OSCSerial updated
    Added functionality to OSCSerial.
    Still need to send feedback of sensors and positions of motors.

--- Arduino/OSCtoSerial/OSCtoSerial.ino
+++ Arduino/OSCtoSerial/OSCtoSerial.ino
@@ -6,13 +6,23 @@
 #include <OSCBundle.h>
 #include <OSCBoards.h>
 
-#define SERIALCOMMAND Serial2
+#define DEBUG         
 
+#define HEADSERIAL    Serial1
+#define LEFTSERIAL    Serial2
+#define RIGHTSERIAL   Serial3
+#define HEAD          0
+#define LEFTSIDE      1
+#define RIGHTSIDE     2
+
+HardwareSerial motorCommPort[3] = {HEADSERIAL, LEFTSERIAL, RIGHTSERIAL};
+int whichPort = 0;
+String messageAddress;
 
 EthernetUDP Udp;
 
 //the Arduino's IP
-IPAddress ip(172, 24, 1, 51);
+IPAddress ip(192, 168, 0, 200);
 
 //port numbers
 const unsigned int inPort = 12345;
@@ -22,6 +32,7 @@ const unsigned int outPort = 12346;
 #if defined(__MK20DX128__)
 // Teensy 3 has MAC burned in
 static byte mac[6];
+
 void read(uint8_t word, uint8_t *mac, uint8_t offset) {
   FTFL_FCCOB0 = 0x41;             // Selects the READONCE command
   FTFL_FCCOB1 = word;             // read the given word of read once area
@@ -38,6 +49,7 @@ void read_mac() {
   read(0xe,mac,0);
   read(0xf,mac,3);
 }
+
 #else
 void read_mac() {}
  byte mac[] = {  
@@ -68,12 +80,17 @@ char * numToOSCAddress( int pin){
 void setup() {
       // some local debug -- we can see this in serial port
   Serial.begin(9600);
+  for(int i=0; i<3; i++){
+    motorCommPort[i].begin(115200);
+  }
   
   read_mac();
   Ethernet.begin(mac,ip);
   Udp.begin(inPort);
-  
-  Serial.println("starting");
+
+  Serial.println(F("====================================================================="));
+  Serial.println(F("TONI DOVE :: OSC TO SERIAL"));
+  Serial.println(F("====================================================================="));
 }
 
 void loop(){ 
@@ -90,46 +107,92 @@ void loop(){
 
         if(!newMes.hasError())
         {
-//              Serial.println("Bundle has been received");
-              newMes.route("/head/rotate", headRotate);
-//              newMes.route("/head/pivot", headPivot);
-//              newMes.route("/left/shoulder/rotate", leftShoulderRotate);
-//              newMes.route("/left/shoulder/pivot", leftShoulderPivot);
-//              newMes.route("/left/elbow/rotate", leftElbowRotate);
-//              newMes.route("/left/elbow/pivot", leftElbowPivot);
-//              newMes.route("/right/shoulder/rotate", rightShoulderRotate);
-//              newMes.route("/right/shoulder/pivot", rightShoulderPivot);
-//              newMes.route("/waist", waistRotate);
+            newMes.route("/move", routeMessage);
         }     
     }
 }
 
+void routeMessage(OSCMessage &msg, int addrOffset){
+      int pinMatched;
+      pinMatched = msg.match("/test", addrOffset);
+      if(pinMatched){
+        Serial.println("OSC Comms Functional");
+      }
+      pinMatched = msg.match("/head", addrOffset);
+      if(pinMatched){
+        whichPort = HEAD;
+        if (msg.fullMatch("/rotate", pinMatched+addrOffset)){
+          messageAddress = "H_ROT";
+        } else if (msg.fullMatch("/pivot", pinMatched+addrOffset)){
+          messageAddress = "H_PIV";
+        }
+      }
+      
+      pinMatched = msg.match("/waist", addrOffset);
+      if(pinMatched){
+        whichPort = HEAD;
+        if (msg.fullMatch("/rotate", pinMatched+addrOffset)){
+          messageAddress = "W_ROT";
+        } else if (msg.fullMatch("/pivot", pinMatched+addrOffset)){
+          messageAddress = "W_PIV";
+        }
+      }
 
+      pinMatched = msg.match("/left", addrOffset);
+      if(pinMatched){
+        whichPort = LEFTSIDE;
+        if (msg.fullMatch("/shoulder/rotate", pinMatched+addrOffset)){
+          messageAddress = "S_ROT";
+        } else if (msg.fullMatch("/shoulder/pivot", pinMatched+addrOffset)){
+          messageAddress = "S_PIV";
+        } else if (msg.fullMatch("/elbow/rotate", pinMatched+addrOffset)){
+          messageAddress = "E_ROT";
+        } else if (msg.fullMatch("/elbow/pivot", pinMatched+addrOffset)){
+          messageAddress = "E_PIV";
+        }
+      }
+
+      pinMatched = msg.match("/right", addrOffset);
+      if(pinMatched){
+        whichPort = RIGHTSIDE;
+        if (msg.fullMatch("/shoulder/rotate", pinMatched+addrOffset)){
+          messageAddress = "S_ROT";
+        } else if (msg.fullMatch("/shoulder/pivot", pinMatched+addrOffset)){
+          messageAddress = "S_PIV";
+        } 
+      }
+      #ifdef DEBUG
+      Serial.print("Message Sent: ");Serial.println(messageAddress);
+      #endif
+      moveToPosition(msg, addrOffset, whichPort, messageAddress);
+}
 // same format for all for right now, but  we may need to make specific conditions
 // may also make more sense in a more generalized way to process
-void headRotate(OSCMessage &msg, int addrOffset ){
-  int position = 0;
-  int speed = 0;
+void printAddrOffset(OSCMessage &msg, int addrOffset){
+  Serial.print("Address Offset: ");Serial.print(addrOffset);
+}
+
+void moveToPosition(OSCMessage &msg, int addrOffset, int portNumber, String addr){
+  int nextPosition = 0;
+  int newSpeed = 0;
+  char OSCAddress[40];
   bool valid = true;
 
-  for(int i = 0; i < 2; i++){
-    if(msg.isInt(i)){
-      if(i = 0){
-        position = msg.getInt(i);
-      }
-      else{
-        speed = msg.getInt(i);
-      }
-    }
-    else{
-      valid = false;
-      Serial.print("Unexpected value of, for: ");
-      // maybe print address name and message?
-    }
+  if(msg.isInt(0)){
+      nextPosition = msg.getInt(0);
   }
-
+  else{
+    valid = false;
+    msg.getAddress((char*)OSCAddress, addrOffset);
+    Serial.print("Unexpected value of, for: ");Serial.println(OSCAddress);
+    // maybe print address name and message?
+  }
+    
   if(valid){
-    // send to serial
+    #ifdef DEBUG
+    Serial.print("Next Position For ");Serial.print(addr);Serial.print(" is: ");Serial.println(nextPosition);
+    #endif
+    motorCommPort[portNumber].print(addr);motorCommPort[portNumber].println(nextPosition);
   }
 }
 
